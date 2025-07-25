3cb89df02ce9bcaeafe6912921f38cf6cdae1380
iamseung <seesungseok@naver.com>
2025-07-25 13:17:18 +0900

    ✅ Order 컨트롤러 및 서비스, 도메인 구현 및 테스트

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -1,12 +1,31 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.repository.OrderRepository;
+import sample.cafekiosk.spring.api.repository.ProductRepository;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.product.Product;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class OrderService {
 
-    public void createOrder(OrderCreateRequest request) {
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
+    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
+        List<String> productNumbers = request.getProductNumbers();
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers); // Product
         
+        Order order = Order.create(products, registeredDateTime); // Order
+        Order savedOrder = orderRepository.save(order);
+
+        return OrderResponse.of(savedOrder);
     }
 }
