22135907797d9f73eb5be921b191171cbab1f383
iamseung <seesungseok@naver.com>
2025-07-25 13:16:30 +0900

    ✅ ProductRepository 생성 및 테스트

--- src/main/java/sample/cafekiosk/spring/api/repository/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/api/repository/ProductRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
+
+    List<Product> findAllByProductNumberIn(List<String> productNumbers);
 }
