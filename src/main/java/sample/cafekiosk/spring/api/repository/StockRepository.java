0233416a17c7daa2b3077c1223f1cdca30068f89
iamseung <seesungseok@naver.com>
2025-07-28 13:30:40 +0900

    ✅ Stock Repository 메서드 생성 및 테스트

--- src/main/java/sample/cafekiosk/spring/api/repository/StockRepository.java
+++ src/main/java/sample/cafekiosk/spring/api/repository/StockRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sample.cafekiosk.spring.domain.stock.Stock;
 
+import java.util.List;
+
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    List<Stock> findAllByProductNumberIn(List<String> productNumbers);
 }
