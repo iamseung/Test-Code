8c287bba202e863eb835c3b7e3800716a85c0553
iamseung <seesungseok@naver.com>
2025-07-28 13:30:56 +0900

    ✅ ProductType 메서드 생성 및 테스트, 비즈니스 레이어에 도입

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.repository.OrderRepository;
 import sample.cafekiosk.spring.api.repository.ProductRepository;
+import sample.cafekiosk.spring.api.repository.StockRepository;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,15 +23,30 @@ public class OrderService {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = findProductsBy(productNumbers);
 
         // 재고 차감 체크가 필요한 상품들 Filter
+        List<String> stockProductNumbers = products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .toList();
+
         // 재고 엔티티 조회
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        Map<String, Stock> stockMap = stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
         // 상품별 Counting
+        Map<String, Long> productCounting = stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
         // 재고 차감 시도
+        for(String stockProductNumber : stockProductNumbers) {
+
+        }
 
         Order order = Order.create(products, registeredDateTime); // Order
         Order savedOrder = orderRepository.save(order);
