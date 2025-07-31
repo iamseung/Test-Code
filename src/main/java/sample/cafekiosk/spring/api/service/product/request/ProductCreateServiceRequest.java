841b66cc8c05f31999b6aca07215e87affe4f66e
iamseung <seesungseok@naver.com>
2025-07-31 23:08:22 +0900

    ✅ 서비스 레이어와 컨트롤러 레이어 분리

--- src/main/java/sample/cafekiosk/spring/api/service/product/request/ProductCreateServiceRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/request/ProductCreateServiceRequest.java
@@ -1,8 +1,5 @@
-package sample.cafekiosk.spring.api.controller.product.dto;
+package sample.cafekiosk.spring.api.service.product.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,22 +9,15 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 
 @Getter
 @NoArgsConstructor
-public class ProductCreateRequest {
+public class ProductCreateServiceRequest {
 
-    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
-
-    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
-
-    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
-
-    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
-    public ProductCreateRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+    public ProductCreateServiceRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         this.type = type;
         this.sellingStatus = sellingStatus;
         this.name = name;
