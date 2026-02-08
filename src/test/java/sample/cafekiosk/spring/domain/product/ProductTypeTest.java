533b6d2130121457c34e6f04cc223563a71e7373
iamseung <seesungseok@naver.com>
2026-02-08 11:29:46 +0900

    feat: 상품 재고 테스트, ParameterizedTest

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -1,36 +1,44 @@
 package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTypeTest {
 
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
-    @Test
-    void containsStockType() {
-        // given
-        ProductType givenType = ProductType.HANDMADE;
-
+    @CsvSource({"HANDMADE,false", "BOTTLE,true", "BAKERY,true"})
+    @ParameterizedTest
+    void containsStockType(ProductType givenType, boolean expected) {
         // when
         boolean result = ProductType.containsStockType(givenType);
 
         // then
-        assertThat(result).isFalse();
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> containsStockType2() {
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
     }
 
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
-    @Test
-    void containsStockType2() {
-        // given
-        ProductType givenType = ProductType.BAKERY;
-
+    @MethodSource
+    @ParameterizedTest
+    void containsStockType2(ProductType givenType, boolean expected) {
         // when
         boolean result = ProductType.containsStockType(givenType);
 
         // then
-        assertThat(result).isTrue();
+        assertThat(result).isEqualTo(expected);
     }
 }
